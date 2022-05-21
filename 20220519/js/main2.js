be06b00802a6c0c8dc12c45968cf5129a0e864b9
Kamigami85 <yanghejie.bt04@nycu.edu.tw>
2022-05-21 21:05:55 +0800

    add breakfast shop menu

---
+++ 20220519/js/main2.js
@@ -0,0 +1,36 @@
+const item1 = document.getElementById('item1');
+const item1Num = document.getElementById('item1Num');
+const item1Price = document.getElementById('item1Price');
+
+const item2 = document.getElementById('item2');
+const item2Num = document.getElementById('item2Num');
+const item2Price = document.getElementById('item2Price');
+
+const item3 = document.getElementById('item3');
+const item3Num = document.getElementById('item3Num');
+const item3Price = document.getElementById('item3Price');
+
+const myForm = document.getElementById('myForm');
+const result = document.getElementById('result');
+
+myForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    let item1Val = parseInt(item1.value);
+    let item1NumVal = parseInt(item1Num.value);
+    let item1Result = item1Val * item1NumVal ;
+    item1Price.value = item1Result;
+
+    let item2Val = parseInt(item2.value);
+    let item2NumVal = parseInt(item2Num.value);
+    let item2Result = item2Val * item2NumVal ;
+    item2Price.value = item2Result;
+    
+    let item3Val = parseInt(item3.value);
+    let item3NumVal = parseInt(item3Num.value);
+    let item3Result = item3Val * item3NumVal ;
+    item3Price.value = item3Result;
+    result4 = item3Result + item2Result + item1Result;
+    result.innerText = "Total price : "+result4 +" dollars";
+    
+})
+ 
